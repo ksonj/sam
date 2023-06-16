4356ebeef56817b522f477ac489ca49843181b49
Kalle Jepsen <git@ksonj.net>
2023-06-16 15:45:57 +0200

    Build via overlay

--- flake.nix
+++ flake.nix
@@ -8,41 +8,18 @@
     self,
     nixpkgs,
     flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
+  }: let
+    overlay = import ./overlay.nix;
+  in
+    {
+      overlays.default = overlay;
+    }
+    // flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux"] (system: let
       pkgs = import nixpkgs {
         inherit system;
+        overlays = [overlay];
       };
-      sam = with pkgs;
-        stdenv.mkDerivation {
-          pname = "sam";
-          version = "2022-11-21-r3";
-          nativeBuildInputs = [autoPatchelfHook makeWrapper];
-          buildInputs = [
-            libsecret
-            curl
-            gtk2
-            xorg.libXxf86vm
-            xorg.libXtst
-            xorg.libSM
-            gfortran6.cc.lib
-          ];
-          src = pkgs.fetchurl {
-            url = "https://sam.nrel.gov/download/71-sam-2022-11-21-for-linux/file.html";
-            sha256 = "22235de1243d56f6295380b1f622de3160f39912c14848aa5e4f364ce01674c6";
-          };
-          unpackPhase = ''
-            cp $src $name.run
-            chmod +x $name.run
-          '';
-          installPhase = ''
-            mkdir -p $out/bin
-            mkdir -p $out/build
-            echo "$out/build" | ./$name.run
-            cp $out/build/SAM $out/bin/sam
-          '';
-        };
     in {
-      packages.default = sam;
+      packages.default = pkgs.sam;
     });
 }
---
+++ overlay.nix
@@ -0,0 +1,30 @@
+final: prev: {
+  sam = prev.stdenv.mkDerivation {
+    pname = "sam";
+    version = "2023-11-21-r3";
+    nativeBuildInputs = [final.autoPatchelfHook final.makeWrapper];
+    buildInputs = with final; [
+      libsecret
+      curl
+      gtk2
+      xorg.libXxf86vm
+      xorg.libXtst
+      xorg.libSM
+      gfortran6.cc.lib
+    ];
+    src = prev.fetchurl {
+      url = "https://sam.nrel.gov/download/71-sam-2022-11-21-for-linux/file.html";
+      sha256 = "22235de1243d56f6295380b1f622de3160f39912c14848aa5e4f364ce01674c6";
+    };
+    unpackPhase = ''
+      cp $src $name.run
+      chmod +x $name.run
+    '';
+    installPhase = ''
+      mkdir -p $out/bin
+      mkdir -p $out/build
+      echo "$out/build" | ./$name.run
+      cp $out/build/SAM $out/bin/sam
+    '';
+  };
+}
